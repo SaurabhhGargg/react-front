48a7856b660b9a9343aea3c276a56add44fce05f
Saurabh Garg <sforsaurabh01@gmail.com>
2021-05-13 15:42:22 +0530

    Initial commit

---
+++ src/services/ecommService.jsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+const url='http://localhost:8081';
+
+class EcommService
+{
+    getProduct()
+    {
+        return axios.get(`${url}/product`);
+    }
+
+    getProductDetails(id)
+    {
+        return axios.get(`${url}/product/${id}`);
+    }
+    getAccountInfo()
+    {
+        return axios.get(`${url}/account`);
+    }
+    getAccountDetails(email)
+    {
+        return axios.get(`${url}/user/${email}`);
+    }
+
+    
+}
+export default new EcommService();
